6d3b407183acf59e187d7bfbbe1bcb6e854884be
harry1878 <kwakju1878@gmail.com>
2020-02-16 16:08:54 +0900

    AOS

---
+++ Assets/Scripts/CameraModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraModule : MonoBehaviour
+{
+    public float distance = 10f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            var player = FindObjectOfType<CharacterModule>();
+
+            transform.position = new Vector3(
+                player.transform.position.x,
+                distance,
+                player.transform.position.z -15f);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterModule.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModule : MonoBehaviour
+{
+    private Camera mainCamera;
+    public float speed = 2.0f;
+    private Rigidbody rigid;
+    private Vector3 targetPosition = Vector3.zero;
+    private Animator animator;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        rigid = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                targetPosition = hit.point;
+                animator.SetBool("isRun", true);
+            }
+        }
+
+        if (Run())
+            Trun(targetPosition);
+
+    }
+
+    private void Trun(Vector3 target)
+    {
+        Vector3 dir = target - transform.position;
+        Vector3 rXZ = new Vector3(dir.x, 0, dir.z);
+
+        Quaternion rot = Quaternion.LookRotation(rXZ);
+        rigid.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            rot,
+            550.0f * Time.deltaTime);
+    }
+
+    private bool Run()
+    {
+        if (targetPosition.Equals(Vector3.zero)) return false;
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        if (distance > 0.01f)
+        {
+            transform.localPosition = Vector3.MoveTowards(
+                transform.position,
+                targetPosition,
+                speed * Time.deltaTime);
+
+            return true;
+        }
+
+        targetPosition = Vector3.zero;
+        animator.SetBool("isRun", false);
+        return false;
+    }
+}
